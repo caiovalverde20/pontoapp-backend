3089081d6d4afbbde130f5969a00279c9d22b78b
caiovalverde20 <caiovalverde20@gmail.com>
2025-01-10 22:52:14 -0300

    Adicionado cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import router from "./routes";
 import { AppDataSource } from "./database";
+import cors from "cors";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 AppDataSource.initialize();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", router);
 
